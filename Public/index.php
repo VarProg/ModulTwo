37682cc0610f4b0e2531ad9adc34f60621e7da84
Aleksandr <94639022+VarDiamond@users.noreply.github.com>
2021-11-23 20:17:46 +0400

    Add files via upload

---
+++ Public/index.php
@@ -0,0 +1,9 @@
+<?php 
+
+require '../vendor/autoload.php';
+
+// Create new Plates instance
+$templates = new League\Plates\Engine('../app/views');
+
+// Render a template
+echo $templates->render('about', ['title' => 'Aleksandr']);
