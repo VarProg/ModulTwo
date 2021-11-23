6b68bda81f4b2c24e6002e71102e0dc20ab169fc
Aleksandr <94639022+VarDiamond@users.noreply.github.com>
2021-11-23 20:21:00 +0400

    Delete Public directory

--- Public/index.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-require '../vendor/autoload.php';
-
-// Create new Plates instance
-$templates = new League\Plates\Engine('../app/views');
-
-// Render a template
-echo $templates->render('about', ['title' => 'Aleksandr']);
