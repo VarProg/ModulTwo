7826f706a171d3fa20a3936820b2ece7702db494
Aleksandr <94639022+VarDiamond@users.noreply.github.com>
2021-12-01 01:43:45 +0400

    Delete app directory

--- app/views/about.php
+++
@@ -1,6 +0,0 @@
-<?php $this->layout('layout', ['title' => 'User Profile']) ?>
-
-<h1>About page</h1>
-<?php foreach($userInViews as $user):?>
-<?php echo $user;?><br>
-<?php endforeach;?>
--- app/views/homepage.php
+++
@@ -1,7 +0,0 @@
-<?php $this->layout('layout', ['title' => 'User Profile']) ?>
-
-<h1>User Profile</h1>
-
-<?php foreach($postsInViews as $posts):?>
-<?php echo $posts['title'];?><br>
-<?php endforeach;?>
